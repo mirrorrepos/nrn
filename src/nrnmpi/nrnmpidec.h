70814ab0a71a495ca5b90e91867cf42cefcba92a
Luc Grosheintz <luc.grosheintz@gmail.com>
2023-07-19 08:15:51 +0200

    Squashed `1uc/memory_usage`.
    This commit adds some printf debugging output, but it already has
    its own PR.

--- src/nrnmpi/nrnmpidec.h
+++ src/nrnmpi/nrnmpidec.h
@@ -1,6 +1,7 @@
 /*
-This file is processed by mkdynam.sh and so it is important that
-the prototypes be of the form "type foo(type arg, ...)"
+This file is processed by mkdynam.sh and so it is important that the prototypes
+be of the form "type foo(type arg, ...)". Moreover, the * needs to be attached
+to the type, e.g. `T*` is valid, but `T *` isn't.
 */
 
 #ifndef nrnmpidec_h
@@ -21,6 +22,11 @@ typedef struct bbsmpibuf {
     int keypos;
     int refcount;
 } bbsmpibuf;
+
+namespace neuron::container {
+struct MemoryStats;
+struct MemoryUsage;
+}  // namespace neuron::container
 
 // olupton 2022-07-06: dynamic MPI needs to dlopen some of these (slightly
 // redefined) symbol names, so keep C linkage for simplicity
@@ -63,6 +69,9 @@ extern void nrnmpi_abort(int errcode);
 extern void nrnmpi_subworld_size(int n);
 extern void nrnmpi_get_subworld_info(int* cnt, int* index, int* rank, int* numprocs, int* numprocs_world);
 
+/* from memory_usage.cpp */
+extern void nrnmpi_memory_stats(neuron::container::MemoryStats& stats, neuron::container::MemoryUsage const& usage);
+extern void nrnmpi_print_memory_stats(neuron::container::MemoryStats const& stats);
 
 /* from mpispike.cpp */
 extern void nrnmpi_spike_initialize();
